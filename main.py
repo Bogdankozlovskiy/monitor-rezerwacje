24bd2b3e9062317675473883a8c5de826c47b7e7
Bogdankozlovskiy <bkozlovsky@bk.ru>
2023-08-06 12:31:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
-from requests import Session
-from requests.adapters import HTTPAdapter
-import os
-from time import sleep
-import warnings
-from datetime import datetime
+from httpx import AsyncClient, AsyncHTTPTransport
+from asyncio import gather
 import webbrowser
-warnings.filterwarnings("ignore")
+import re
 
 
-
-required_termin = "2023-08-09"
-session = Session()
-session.mount('https://', HTTPAdapter(max_retries=25))
-session.get("https://rezerwacje.duw.pl/pol/login", verify=False, timeout=120)
-session.post(
-    "https://rezerwacje.duw.pl/pol/login", 
-    data={
-        "data[_Token][key]": "abd7ebbf52babfc3175e6056c25ce7a5fcde12dd",
-        "data[User][email]": "login",
-        "data[User][password]": "password",
-        "data[_Token][fields]": "950d15a2cfde63e0ea9e1fa1b85587c9ff982433%3AUser.return_to"
-    },
-    timeout=120,
-    verify=False
-)
-previous_page = None
+# data_template = {"time": "2023-08-14 {time}", "queue": 112, "schedule": 29}
+data_template = {"time": "2023-08-14 {time}", "queue": 100, "schedule": 28} # TEST !!
+time_arr = ["09:00", "09:10", "09:20", "09:30", "09:40", "09:50"]
+url = "https://rezerwacje.duw.pl/reservations/updateFormData/{order_id}/{schedule}"
+loading = ["-", "\\", "|", "/"]
+locked_ids = []
+states = ["-" for _ in enumerate(time_arr)]
 
 
-while True:
-    response = session.get(
-        f"https://rezerwacje.duw.pl/pol/queues/112/29/{required_termin}",
-        timeout=120,
-        verify=False
+async def worker(client, worker_index, time):
+    index = 1
+    data = data_template.copy()
+    data["time"] = data["time"].format(time=time)
+    while not locked_ids:
+        try:
+            response = await client.post("https://rezerwacje.duw.pl/reservations/lock", data=data)
+        except Exception as exc:
+            states[worker_index] = "*"
+        else:
+            if pattern := re.match(r"^OK (?P<order_id>\d*)$", response.text):
+                order_id=pattern.groupdict()["order_id"]
+                states[worker_index] = f"{time}/{order_id} "
+                locked_ids.append(order_id)
+            else:
+                states[worker_index] = loading[index % len(loading)]
+                index += 1
+        print("\r", ' '.join(states), end='')
+
+
+async with AsyncClient(transport=AsyncHTTPTransport(verify=False, retries=50)) as client:
+    await client.get("https://rezerwacje.duw.pl/pol/login")
+    await client.post(
+        "https://rezerwacje.duw.pl/pol/login",
+        data={
+            "data[_Token][key]": "abd7ebbf52babfc3175e6056c25ce7a5fcde12dd",
+            "data[User][email]": "email@email.email",
+            "data[User][password]": "password@passwrod",
+            "data[_Token][fields]": "950d15a2cfde63e0ea9e1fa1b85587c9ff982433%3AUser.return_to"
+        }
     )
-    if previous_page:
-        os.remove(previous_page)
-    new_page = f"/Users/bogdankozlovsky/Desktop/page-{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.html"
-    with open(new_page, "w") as file:
-        previous_page = new_page
-        file.write(response.text)
-    if "WoW" not in response.text and "The specified URL cannot be found." not in response.text:
-        break
-    sleep(10)
+    await gather(*[worker(client, index, time) for index, time in enumerate(time_arr)])
+    final_url = url.format(order_id=locked_ids[-1], schedule=data_template["schedule"])
+    webbrowser.open(final_url)
+    
 
-webbrowser.open(f"https://rezerwacje.duw.pl/pol/queues/112/29/{required_termin}")
-
-while True:
-    os.system('say "Beer time."')
-    sleep(2)
+# 92012416933
