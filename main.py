7a6bc9d148d129769f0fdd9cb8e5b17f098060d6
Bogdankozlovskiy <bkozlovsky@bk.ru>
2023-08-09 22:11:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,62 @@
-from httpx import AsyncClient, AsyncHTTPTransport
-from asyncio import gather
-import webbrowser
+from requests import Session
+from warnings import simplefilter
 import re
 
 
-data_template = {"time": "2023-08-14 {time}", "queue": 112, "schedule": 29}
-time_arr = ["09:00", "09:10", "09:20", "09:30", "09:40", "09:50"]
-url = "https://rezerwacje.duw.pl/reservations/updateFormData/{order_id}/{schedule}"
-loading = ["-", "\\", "|", "/"]
-locked_ids = []
-states = ["-"] * len(time_arr)
+simplefilter("ignore")
+statuses = ["-", "\\", "|", "/"]
+index = 0
+white = " " * 30
+
+data = {"time": "2023-08-17 11:30:00", "queue": 112, "schedule": 26}
+json_data = [{"name": "Imię", "value": "<your first name>"}, {"name": "Nazwisko", "value": "<your last name>"}]
+url_template_1 = "https://rezerwacje.duw.pl/reservations/updateFormData/{order_id}/{schedule}"
+url_template_2 = "https://rezerwacje.duw.pl/reservations/reserv/{order_id}/{schedule}"
 
 
-async def worker(client, worker_index, time):
-    index = 1
-    data = data_template.copy()
-    data["time"] = data["time"].format(time=time)
-    while not locked_ids:
-        try:
-            response = await client.post("https://rezerwacje.duw.pl/reservations/lock", data=data)
-        except Exception as exc:
-            states[worker_index] = f"{time} * "
-        else:
-            if pattern := re.match(r"^OK (?P<order_id>\d*)$", response.text):
-                order_id = pattern.groupdict()["order_id"]
-                states[worker_index] = f"{time} {order_id} "
-                locked_ids.append(order_id)
-            else:
-                states[worker_index] = f"{time} {loading[index % len(loading)]} "
-                index += 1
-        print("\r", ' '.join(states), end='')
+client = Session()
+client.get("https://rezerwacje.duw.pl/pol/login", verify=False)
+client.post(
+    "https://rezerwacje.duw.pl/pol/login",
+    verify=False,
+    data={
+        "data[_Token][key]": "abd7ebbf52babfc3175e6056c25ce7a5fcde12dd",
+        "data[User][email]": "email@email.xuy",
+        "data[User][password]": "passwordpasswrod",
+        "data[_Token][fields]": "950d15a2cfde63e0ea9e1fa1b85587c9ff982433%3AUser.return_to"
+    }
+)
 
-
-async with AsyncClient(transport=AsyncHTTPTransport(verify=False, retries=50), timeout=30) as client:
-    await client.get("https://rezerwacje.duw.pl/pol/login")
-    await client.post(
-        "https://rezerwacje.duw.pl/pol/login",
-        data={
-            "data[_Token][key]": "abd7ebbf52babfc3175e6056c25ce7a5fcde12dd",
-            "data[User][email]": "email@email.email",
-            "data[User][password]": "pssword@paswword",
-            "data[_Token][fields]": "950d15a2cfde63e0ea9e1fa1b85587c9ff982433%3AUser.return_to"
-        }
-    )
-    await gather(*[worker(client, index, time) for index, time in enumerate(time_arr)])
-    final_url = url.format(order_id=locked_ids[-1], schedule=data_template["schedule"])
-    webbrowser.open(final_url)
+while True:
+    index += 1
+    try:
+        response = client.post("https://rezerwacje.duw.pl/reservations/lock", data=data, verify=False)
+        print("\r step: 1", response.text, statuses[index % len(statuses)], white, end="")
+    except:
+        print("\r 1", "step: error", statuses[index % len(statuses)], white, end="")
+        continue
+    if pattern := re.match(r"^OK (?P<order_id>\d*)$", response.text):
+        order_id = pattern.groupdict()["order_id"]
+        url_1 = url_template_1.format(order_id=order_id, schedule=data["schedule"])
+        url_2 = url_template_2.format(order_id=order_id, schedule=data["schedule"])
+        while True:
+            try:
+                response = client.post(url_1, json=json_data, verify=False)
+                print("\r step: 2", response.text.split("\n")[0], statuses[index % len(statuses)], white, end="")
+            except:
+                print("\r step: 2", "error", statuses[index % len(statuses)], white, end="")
+                continue
+            if response.ok:
+                break
+        while True:
+            try:
+                response = client.get(url_2, verify=False)
+                print("\r step: 3", response.text.split("\n")[0], statuses[index % len(statuses)], white, end="")
+            except:
+                print("\r step: 3", "error", statuses[index % len(statuses)], white, end="")
+                continue
+            if response.ok:
+                break
+        break
+        
+print("\r Done !!!")
